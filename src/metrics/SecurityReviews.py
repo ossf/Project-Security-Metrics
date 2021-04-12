6d7b5ab739d426c1eec805af25cc03eb1999a958
Dilan <dbhalla21@berkeley.edu>
2021-04-11 21:31:34 -0700

    converting security reviews section to table of reviews

--- src/metrics/SecurityReviews.py
+++ src/metrics/SecurityReviews.py
@@ -8,12 +8,10 @@ import re
 import shutil
 import subprocess
 
-import dateutil
 import requests
-from dateutil.parser import parse
-from packageurl.contrib import purl2url
 
 from .Base import BaseJob
+
 
 class RefreshSecurityReviews(BaseJob):
     SECURITY_REVIEW_REPO_URL = "https://github.com/ossf/security-reviews.git"
@@ -27,24 +25,25 @@ class RefreshSecurityReviews(BaseJob):
         res = subprocess.check_output(
             ["git", "clone", "--depth", "1", self.SECURITY_REVIEW_REPO_URL]
         )
-        if not os.path.isdir("security-reviews"):
-            logging.warning("Missing repository, clone likely failed.")
-            return
-        for root, _, files in os.walk("security-reviews/reviews", topdown=False):
-            for name in files:
-                self.process_file(os.path.join(root, name))
-        payload_values = [v for _, v in self._payload.items()]
-        # print(payload_values)
-        res = requests.post(self.METRIC_API_ENDPOINT, json=payload_values, timeout=120)
-        if res.status_code == 200:
-            logging.info("Success: %s", res.text)
-        else:
-            logging.warning("Failure: status code: %s", res.status_code)
-        shutil.rmtree("security-reviews")
+        try:
+            if not os.path.isdir("security-reviews"):
+                logging.warning("Missing repository, clone likely failed.")
+                return
+            for root, _, files in os.walk("security-reviews/reviews", topdown=False):
+                for name in files:
+                    self.process_file(os.path.join(root, name))
+            payload_values = [v for _, v in self._payload.items()]
+            res = requests.post(self.METRIC_API_ENDPOINT, json=payload_values, timeout=120)
+            if res.status_code == 200:
+                logging.info("Success: %s", res.text)
+            else:
+                logging.warning("Failure: status code: %s", res.status_code)
+        finally:
+            shutil.rmtree("security-reviews")
 
     def process_file(self, filename):
         validate_file(filename)
-        header, body, section, methodology = {"- pkg": [], "Methodology": [], "author": []}, [], None, False
+        header, body, section, methodology = {"- pkg": [], "Methodology": [], "author": [], "date": []}, [], None, False
         with open(filename, "r") as f:
             lines = f.readlines()
         # for each line, append to header if line in top metadata, append to body if line is past this section
@@ -64,13 +63,16 @@ class RefreshSecurityReviews(BaseJob):
                 body.append(line)
 
         # format lists as strings for grafana UI
-        header["Methodology"] = ", ".join(header["Methodology"]) if header["Methodology"] else "None listed"
         header["author"] = ", ".join(header["author"]) if header["author"] else "None listed"
-        body = "\n".join(body).strip()
+        header["date"] = ", ".join(header["date"]) if header["date"] else "None listed"
+        header["Methodology"] = ", ".join(header["Methodology"]) if header["Methodology"] else "None listed"
+
+        body = "\n".join(body).strip().replace("###", "####")
+        link = "https://www.github.com/ossf/" + filename
 
         # add the header and body to the payload for the provided package url
         for package_url in header.get("- pkg", []):
-            create_payload(self, package_url, header, body)
+            create_payload(self, package_url, header, link)
 
 def validate_file(filename):
     if not os.path.isfile(filename):
@@ -91,6 +93,8 @@ def append_to_header(key, value, methodology, header):
         header[key] = value
     if key == "- Name" or key == "- Organization":
         header["author"].append(value)
+    if key == "Review-Date":
+        header["date"].append(value)
 
 # retrieves the key and value of the header item, and whether the item is within the methodology section
 def get_key_val_methodology(line, methodology):
@@ -106,8 +110,8 @@ def get_key_val_methodology(line, methodology):
     key = parts[0].strip()
     return key, value, methodology
 
-# Takes a package url, the header info, and body and sets up the payload
-def create_payload(self, package_url, header, body):
+# Takes a package url, the header info, and link and sets up the payload
+def create_payload(self, package_url, header, link):
     if package_url not in self._payload:
         self._payload[package_url] = {
             "package_url": "pkg:" + package_url.split("@")[0],
@@ -115,16 +119,13 @@ def create_payload(self, package_url, header, body):
             "key": "security-review",
             "values": [],
         }
-    properties = {"review-text": body}
-    value = "No recommendation"
+    properties = {"review-text": link}
     for k, v in header.items():
-        if k == "recommendation":
-            value = v.strip()
-        elif k == "- pkg":
+        if k == "- pkg":
             continue
         else:
             properties[k.strip()] = str(v)
-    self._payload[package_url]["values"].append({"value": value, "properties": properties})
+    self._payload[package_url]["values"].append({"value": "", "properties": properties})
 
 
 if __name__ == "__main__":
