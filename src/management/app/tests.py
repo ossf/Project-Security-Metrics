48ae0a73b2f86e5cde1af1107150bcc2cdf19221
Chuya Goto <chuya.goto@trustdock.io>
2021-09-19 15:40:10 +0900

    fix: Add test to check response of metric asoosiated package

--- src/management/app/tests.py
+++ src/management/app/tests.py
@@ -1,19 +1,39 @@
 import json
 from django.test import TestCase
 from django.test import Client
-from app.models import Package
+from app.models import Package, Metric
 client = Client()
 
 class ViewApiGetPackageTests(TestCase):
     
     registered_package_url = "pkg:github/rails/rails"
     registered_url = "https://github.com/rails/rails"
+    metric_key = "openssf.security-review"
+    metric_value = "test"
+    metric_properties = "test"
+    
     def setUp(self):
-        Package.objects.create(package_url=self.registered_package_url)
+        package = Package.objects.create(package_url=self.registered_package_url)
+        metric = Metric.objects.create(
+            package=package, key=self.metric_key, value=self.metric_value, properties=self.metric_properties
+        )
 
     def test_valid_package_url(self):
         response = client.get(f"/api/1/get-project?package_url={self.registered_package_url}")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            json.loads(response.content),
+            {
+                "package_url": self.registered_package_url,
+                "metrics": [
+                    {
+                        "key": self.metric_key,
+                        "value": self.metric_value,
+                        "properties": self.metric_properties
+                    }
+                ]
+            }
+        )
 
     def test_valid_url(self):
         response = client.get(f"/api/1/get-project?url={self.registered_url}")
